41304651b70d663a08ad2001d3cb3728688d982e
acdoorn <ac.vandoorn@student.avans.nl>
2014-01-21 13:52:44 +0100

    extra

---
+++ Barometer/Controllers/MentorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barometer.Controllers
+{
+    public class MentorController : Controller
+    {
+        //
+        // GET: /Mentor/
+
+        public ActionResult SelectClass()
+        {
+            return View();
+        }
+
+        public ActionResult SelectStudent()//selecteer student om de voortgang te zien
+        {
+            return View(); 
+        }
+
+        public ActionResult ShowStats()//laat voortgang van geselecteerde studenten zien
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Barometer/Controllers/ProjectDocentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barometer.Controllers
+{
+    public class ProjectDocentController : Controller
+    {
+        //
+        // GET: /ProjectDocent/
+
+        public ActionResult MakeProject()
+        {
+            return View();
+        }
+
+        public ActionResult MakeQuestionList()
+        {
+            return View();
+        }
+
+        public ActionResult CheckProjectGroups()
+        {
+            return View();
+        }
+
+        public ActionResult AddTutorToGroup()
+        {
+            return View();
+        }
+
+        public ActionResult DetermineFillDates()
+        {
+            return View();
+        }
+
+    }
+}
