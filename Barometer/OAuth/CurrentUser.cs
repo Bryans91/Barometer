1e9ccad654023ca186daecd46fe79a74937ecb99
Thomas Voogt <chirimorin@gmail.com>
2014-01-27 11:52:06 +0100

    Login als student

---
+++ Barometer/OAuth/CurrentUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Barometer.OAuth
+{
+    public class CurrentUser
+    {
+        public int ID { get; set; }
+        public string DisplayName { get; set; }
+        public access Access { get; set; }
+    }
+
+    public enum access
+    {
+        student,
+        projectDocent,
+        tutor,
+        mentor
+    }
+}
