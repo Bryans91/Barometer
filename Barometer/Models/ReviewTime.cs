122e04630bc9fc1fd7e164b6418b876ad79d10dc
jertje260 <j_w_broekhuizen@hotmail.com>
2014-01-27 21:39:54 +0100

    ReviewTime added, nog niet compleet. maak het nog wel af.

---
+++ Barometer/Models/ReviewTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Barometer.Models
+{
+    public class ReviewTime
+    {
+        private Student _reviewee;
+        private Student _reviewedStudent;
+        private Project _project;
+        private DateTime _reviewDate;
+
+        public DateTime ReviewDate
+        {
+            get { return _reviewDate; }
+            set { _reviewDate = value; }
+        }
+        
+
+        public Project ReviewedProject
+        {
+            get { return _project; }
+            set { _project = value; }
+        }
+        
+
+        public Student RatedStudent
+        {
+            get { return _reviewedStudent; }
+            set { _reviewedStudent = value; }
+        }
+        
+
+        public Student Reviewee
+        {
+            get { return _reviewee; }
+            set { _reviewee = value; }
+        }
+        
+    }
+}
